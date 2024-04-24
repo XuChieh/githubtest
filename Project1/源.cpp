201ab231363b1dfdabd21e789aef14a9951470e3
XuChieh <XuChieh@XUCHIEH>
2024-04-24 22:06:31 +0800

    添加项目文件。

---
+++ Project1/源.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 声明两个变量来存储用户输入的数字
+    int num1, num2;
+
+    // 提示用户输入第一个数字
+    cout << "请输入第一个
+    cin >> num1;
+
+    // 提示用户输入第二个数字
+    cout << "请输入第二个数字: ";
+    cin >> num2;
+
+    // 计算两个数字的和
+    int sum = num1 + num2;
+
+    // 输出结果
+    cout << "这两个数字的和是: " << sum << endl;
+
+    return 0;
+}
